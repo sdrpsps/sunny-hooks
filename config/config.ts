9c0272d5dda63d46583297d5c5663b6f3a1b9378
sunny <sunny@bytespark.app>
2024-03-06 11:21:12 +0800

    init: 项目初始化

---
+++ config/config.ts
@@ -0,0 +1,57 @@
+export default {
+  exportStatic: {},
+  nodeModulesTransform: {
+    type: 'none',
+    exclude: [],
+  },
+  history: {
+    type: 'hash',
+  },
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: '@alifd/next',
+        style: false,
+      },
+      'fusion',
+    ],
+  ],
+  mode: 'site',
+  title: 'Sunny React Hooks',
+  logo: '/logo.png',
+  dynamicImport: {},
+  manifest: {},
+  hash: true,
+  alias: {
+    encodeHooks: `${process.cwd()}/packages/hooks/src/index.ts`,
+  },
+  resolve: {
+    includes: ['docs', 'packages/hooks/src'],
+  },
+  links: [
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/@alifd/theme-design-pro@0.6.2/dist/next-noreset.min.css',
+    },
+  ],
+  navs: [
+    { title: '指南', path: '/guide' },
+    { title: 'Hooks', path: '/hooks' },
+  ],
+  menus: {
+    '/': [
+      {
+        title: '首页',
+        path: 'index',
+      },
+    ],
+    '/guide': [
+      {
+        title: '介绍',
+        path: '/guide',
+      },
+    ],
+    // '/hooks': menus,
+  },
+};
